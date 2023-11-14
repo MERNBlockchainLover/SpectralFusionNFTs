15720e30f160a3943b24fd4a84f3289a289b805e
MERNBlockchainLover <150639064+MERNBlockchainLover@users.noreply.github.com>
2023-11-14 09:40:29 -0800

    update backend

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/images', express.static('imgs'));
 const routes = require('./routes')(app);
 
+app.use(express.static('build'));
+app.get('*', function(req, res) {
+  res.sendFile('index.html', { root: __dirname })
+});
+
 const server = app.listen(3333, () => {
   console.log('Listening on port %s...', server.address().port);
 });
